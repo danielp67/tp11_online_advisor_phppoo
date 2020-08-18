210628d17dcc4ef85528d489dca31e135ef54669
danielp67 <daniel.67100@hotmail.fr>
2020-08-18 13:48:25 +0200

    initialization

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+
+
+// Récupération des blocs
+const mainMenu = document.querySelector("#nav_menu");
+const burgerMenu = document.querySelector("#menu_burger");
+
+
+burgerMenu.addEventListener("click", function() {
+	
+	mainMenu.classList.toggle("active");
+
+});
+
+
+const liens =document.querySelectorAll("li");
+
+liens.forEach(lien =>{
+
+	lien.addEventListener("click", (e)=>{
+		mainMenu.classList.remove("active");
+		e.preventDefault();
+		e.stopPropagation();
+	})
+})
